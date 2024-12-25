01b10a52b47afcfadc36d1ea58bb5418b0338d79
Prahaladha-Reddy <prahaladreddy80@gmail.com>
2024-12-25 23:01:03 +0530

    Base model preparation is complete by using vgg16

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_1_data_ingestion import *
 
+from src.cnnClassifier.pipeline.stage_02_prepare_model import *
 STAGE_NAME='Data Ingestion'
 
 try:
   logger.info(f'>>> Stage {STAGE_NAME} started <<<')
   obj=DataIngestionTrainingPipeline()
+  obj.main()
+  logger.info(f'>> stage {STAGE_NAME} completed <<')
+except Exception as e:
+  logger.exception(e)
+  raise e
+
+
+try:
+  logger.info(f'>>> Stage {STAGE_NAME} started <<<')
+  obj=PrepareBaseModelTrainingPipeline()
   obj.main()
   logger.info(f'>> stage {STAGE_NAME} completed <<')
 except Exception as e:
